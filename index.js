561fc07909188725a0ad88eee822ac3a8dea8810
Mo <mo.abdelnaim@gmail.com>
2023-05-01 13:42:35 +0300

    added muhammed's code & gitignore

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ const auth = require("./routes/Auth");
 const busroutes = require("./routes/busroutes");
 const users = require("./routes/users");
 const requests = require("./routes/requests");
-
-
+const appointments = require("./routes/appointments");
 
 app.listen(4000, "localhost", () => {
   console.log("SERVER IS RUNNING ");
@@ -26,3 +25,4 @@ app.use("/auth", auth);
 app.use("/busroutes", busroutes);
 app.use("/users", users);
 app.use("/requests", requests);
+app.use("/appointments", appointments);
